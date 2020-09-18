bab8292216e14b5263d5c827f7261f910486f3e2
kattmeow <52311570+kattmeow@users.noreply.github.com>
2020-09-18 16:21:21 +0300

    Update change-ssl3-tls12.ps1

--- change-ssl3-tls12.ps1
+++ change-ssl3-tls12.ps1
@@ -4,7 +4,6 @@ if (! (Test-Path $ProfileFile)) {
 New-Item -Path $ProfileFile -Type file -Force
 }
 ''                                                                                | Out-File -FilePath $ProfileFile -Encoding ascii -Append
-'# It is 2020, SSL3 and TLS 1.0 are no good anymore'                              | Out-File -FilePath $ProfileFile -Encoding ascii -Append
 '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12' | Out-File -FilePath $ProfileFile -Encoding ascii -Append
 
 notepad $ProfileFile
